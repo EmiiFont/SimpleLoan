d9f5e2bf046dd3542b2701aa6d5884a6573fc9ea
emilio font <emiliofont@emilios-MacBook-Pro.local>
2019-03-25 21:51:40 -0400

    added missing address module

--- SimpleLoans/Models/Address.cs
+++ SimpleLoans/Models/Address.cs
@@ -2,5 +2,10 @@ namespace SimpleLoans.Models
 {
     public class Address
     {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Sector { get; set; }
+        public string Province { get; set; }
+        public int ApartmentNumber { get; set; }
     }
 }
