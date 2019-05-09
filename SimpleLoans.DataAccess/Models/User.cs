7fb5e6254c9faadca64112c05da0fa1eec82b173
emilio font <emiliofont@emilios-MacBook-Pro.local>
2019-05-08 23:27:23 -0400

    fixed user model

--- SimpleLoans.DataAccess/Models/User.cs
+++ SimpleLoans.DataAccess/Models/User.cs
@@ -5,7 +5,7 @@ namespace SimpleLoans.DataAccess.Models
         public string FirstName { get; set; }
         public string  LastName { get; set; }
         public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordHash { get; internal set; }
         public byte[] PasswordSalt { get; set; }
     }
 }
